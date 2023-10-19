5659857b37126da08f503f356a44371a9d7f4e10
Mahe Tardy <mahe.tardy@gmail.com>
2023-10-19 15:07:25 +0000

    bpf: copy path info from excve_event to exceve_map
    This copies the information to persist in the execve_map.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- bpf/process/bpf_execve_event.c
+++ bpf/process/bpf_execve_event.c
@@ -287,6 +287,10 @@ execve_send(struct sched_execve_args *ctx)
 			curr->caps.inheritable = event->caps.inheritable;
 		}
 #endif
+		// reuse p->args first string that contains the filename, this can't be
+		// above 256 in size (otherwise the complete will be send via data msg)
+		// which is okay because we need the 256 first bytes.
+		curr->binary_path.path_length = probe_read_str(curr->binary_path.path, BINARY_PATH_MAX_LEN, &(p->args));
 	}
 
 	event->common.flags = 0;
